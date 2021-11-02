79cef05f7b119ad2458a4cdee5e9c5e9750ec608
vinicios <soicinivt@gmail.com>
2021-11-01 21:04:30 -0300

    Change size in loadind.

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Loading extends Component {
   render() {
     return (
-      <div className="text-red-fireEngine">Carregando...</div>
+      <div className="text-red-fireEngine text-4xl">Carregando...</div>
     );
   }
 }
